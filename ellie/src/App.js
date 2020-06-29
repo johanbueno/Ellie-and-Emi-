d69c9fc812c83f7a48e787382564da415f1b0837
Johan Bueno <johan.s.bueno@gmail.com>
2020-06-28 22:13:52 -0400

    navbar and footer

---
+++ ellie/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router} from "react-router-dom";
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar/>
+        <Footer/>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
